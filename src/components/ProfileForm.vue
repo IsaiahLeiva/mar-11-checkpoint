fbb212142b2be41cfde717c2d906b11a440b7386
Isaiah <isaiah.leiva@gmail.com>
2022-03-11 16:51:27 -0700

    commit

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -12,13 +12,34 @@
         />
       </div>
       <div id="bio-field">
-        <input type="text" />
+        <input
+          type="text"
+          v-model="editable.bio"
+          class="form-control"
+          name="bio"
+          id="bio"
+          placeholder="bio..."
+        />
       </div>
       <div id="image-field">
-        <input type="text" />
+        <input
+          type="text"
+          v-model="editable.imgUrl"
+          class="form-control"
+          name="imgUrl"
+          id="imgUrl"
+          placeholder="Upload Image..."
+        />
       </div>
       <div id="email">
-        <input type="text" />
+        <input
+          type="text"
+          v-model="editable.email"
+          class="form-control"
+          name="email"
+          id="email"
+          placeholder="email..."
+        />
       </div>
     </div>
   </form>
