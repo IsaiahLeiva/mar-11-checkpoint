4249606eef74ad203ddea8b60da1ead59f7f4ec6
Isaiah <isaiah.leiva@gmail.com>
2022-03-11 11:53:32 -0700

    commit

---
+++ src/pages/PostPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+    <div class="row w-100 m-2 p-3">
+      <div></div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.profile-img {
+  height: 100px;
+  width: 100px;
+}
+</style>
